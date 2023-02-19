1e27018aff70f9da132aef929efb701e847604f7
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-02-19 14:18:06 +0900

    solve boj 11659

---
+++ src/baekjoon/problem20438/Problem20438.kt
@@ -0,0 +1,8 @@
+package baekjoon.problem20438
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val (N,K,Q,M) = readLine().split(" ").map { it.toInt() }
+    val kList = readLine().split(" ").map { it.toInt() }
+    val qList = readLine().split(" ").map { it.toInt() }
+    
+}
