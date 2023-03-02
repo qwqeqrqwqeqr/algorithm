a68fa8144cdc78717dc32ed2037fd21ca1d350b2
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-03-02 22:05:34 +0900

    sovle boj 4659

--- src/baekjoon/problem1181/Problem1181.kt
+++ src/baekjoon/problem1181/Problem1181.kt
@@ -2,16 +2,15 @@ package baekjoon.problem1181
 
 import java.io.BufferedWriter
 import java.io.OutputStreamWriter
+
 val bufferedWriter = BufferedWriter(OutputStreamWriter(System.out))
-fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
-    val list = ArrayList<String>()
-    repeat(readLine().toInt()) {
-        list.add(readLine())
-    }.run {
-        list.sortedWith(compareBy<String> { it.length }.thenBy { it }).distinct()
-            .forEach { bufferedWriter.append(it + "\n") }.run {
-            bufferedWriter.flush()
-        }
+fun main(args: Array<String>): Unit = with(System.`in`.bufferedReader()) {
+    ArrayList<String>().apply {
+        repeat(readLine().toInt()) { this.add(readLine()) }
+    }.also {
+        it.sortedWith(compareBy<String> { it.length }.thenBy { it }).distinct()
+            .forEach { bufferedWriter.append(it + "\n") }
+        bufferedWriter.flush()
     }
 }
 
