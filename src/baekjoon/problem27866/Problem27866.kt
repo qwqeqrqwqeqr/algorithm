56c82764cfdc3ae1b6cc42fb9832abac8e69c30b
qwqeqrqwqeqr <fancythat0831@gmail.com>
2023-09-10 13:14:13 +0900

    feat(boj): solve boj - Problem27866.kt

---
+++ src/baekjoon/problem27866/Problem27866.kt
@@ -0,0 +1,7 @@
+package baekjoon.problem27866
+
+fun main(args: Array<String>): Unit = with(System.`in`.bufferedReader()) {
+    readLine().apply {
+        println(this[readLine().toInt()-1])
+    }
+}
