29dacc30dec9ffe975e00264a282ba89eef8125e
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-19 11:31:54 +0900

    feat(boj): solve boj - Problem10171

---
+++ src/main/kotlin/baekjoon/problem10171/Problem10171.kt
@@ -0,0 +1,8 @@
+package baekjoon.problem10171
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    println("""\    /\""")
+    println(""" )  ( ')""")
+    println("""(  /  )""")
+    println(""" \(__)|""")
+}
