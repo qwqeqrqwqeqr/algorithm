0075ab0534a3837bbd388485363aa4438a023116
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2023-12-19 11:33:44 +0900

    feat(boj): solve boj - Problem10172

---
+++ src/main/kotlin/baekjoon/problem10172/Problem10172.kt
@@ -0,0 +1,9 @@
+package baekjoon.problem10172
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    println("""|\_/|""")
+    println("""|q p|   /}""")
+    println("""( 0 )""${'"'}\""")
+    println("""|"^"`    |""")
+    println("""||_/=\\__|""")
+}
