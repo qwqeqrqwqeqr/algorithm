2c9e6b52a3025ea955e0c9c5a4114f3c80735a63
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-01-02 16:27:17 +0900

    feat(boj): solve boj - Problem18870

--- src/main/kotlin/baekjoon/problem18870/Problem18870.kt
+++ src/main/kotlin/baekjoon/problem18870/Problem18870.kt
@@ -1,26 +1,12 @@
 package baekjoon.problem18870
 
+
 fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
     readLine()
-    var arrayList = arrayListOf<Coordinate>()
-    readLine().split(" ").map { it.toInt() }.forEachIndexed { index, i ->
-        arrayList.add(Coordinate(i, index, 0))
-    }
-    val tempList = arrayList.sortedBy { it.coordinate }
-
-    var temp = tempList[0].coordinate
-    var count = 0
-    for(i in tempList.indices){
-        if(temp!=tempList[i].coordinate){
-            count++
-        }
-        tempList[i].rank = count
-        temp = tempList[i].coordinate
-    }
-
-    tempList.sortedBy { it.number }.forEach { print("${it.rank} ") }
-
-
+    val list = readLine().split(" ").map { it.toInt() }
+    val map: HashMap<Int, Int> = HashMap()
+    list.sorted().distinct().forEachIndexed { index, i -> map[i] = index }
+    println(list.map { map[it] }.joinToString(" "))
 }
 
-data class Coordinate(val coordinate: Int, val number: Int, var rank: Int)
+
