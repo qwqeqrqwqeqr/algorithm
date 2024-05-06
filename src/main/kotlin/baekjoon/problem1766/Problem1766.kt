c29c024ff617de2137f093840e20ebdf685f073b
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2024-05-06 13:33:42 +0900

    feat(boj): solve boj - Problem1766

---
+++ src/main/kotlin/baekjoon/problem1766/Problem1766.kt
@@ -0,0 +1,32 @@
+package baekjoon.problem1766
+
+
+import java.util.PriorityQueue
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val array = IntArray(n + 1) { 0 }
+    val graph = Array<ArrayList<Int>>(n + 1) { ArrayList() }
+    val queue = PriorityQueue<Int>()
+    val answer = ArrayList<Int>()
+    repeat(m) {
+        val (a, b) = readLine().split(" ").map { it.toInt() }
+        graph[a].add(b)
+        array[b] += 1
+    }
+
+    for (i in 1 .. n) {
+        if (array[i] == 0) queue.add(i)
+    }
+
+    while (queue.isNotEmpty()) {
+        val item = queue.poll()
+        answer.add(item)
+
+        graph[item].forEach {
+            array[it] -= 1
+            if (array[it] == 0) queue.add(it)
+        }
+    }
+    println(answer.joinToString(" "))
+}
